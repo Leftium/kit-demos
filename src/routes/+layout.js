646f311c4fa67a079616c756e480af743c6fe13e
John-Kim <Leftium@users.noreply.github.com>
2022-10-03 23:22:00 +0900

    Workaround for routeId does not invalidate layout.

---
+++ src/routes/+layout.js
@@ -0,0 +1,8 @@
+
+
+export function load({routeId, url}) {
+    return {
+        routeId,
+        url
+    }
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let data
+</script>
+
+<h1>Layout</h1>
+<pre>routeId: {data.routeId}</pre>
+<pre style='display: none;'>url: {data.url}</pre>
+
+<a href='/route-a'>Route A</a>
+<a href='/route-b'>Route B</a>
+
+<slot></slot>
