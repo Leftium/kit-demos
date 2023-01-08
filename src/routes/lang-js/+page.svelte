37f6c64303a80a0b6e6a5a205a22d0ace2969a27
John-Kim <Leftium@users.noreply.github.com>
2023-01-08 14:39:08 +0900

    Make simple ts @hmr:keep reproduction.

---
+++ src/routes/lang-js/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	// @hmr:keep
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<h1>{'<script lang="ts">'}</h1>
+
+<button on:click={handleClick}>
+	clicks: {count}
+</button>
+
+<ol>
+	<li>Click button</li>
+	<li>Change increment size</li>
+	<li>Inspect count (preserved as expected)</li>
+</ol>
---
+++ src/routes/lang-ts/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	// @hmr:keep
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<h1>{'<script lang="ts">'}</h1>
+
+<button on:click={handleClick}>
+	clicks: {count}
+</button>
+
+<ol>
+	<li>Click button</li>
+	<li>Trigger HMR (change increment size)</li>
+	<li>Inspect count (<b>not</b> preserved as expected)</li>
+</ol>
