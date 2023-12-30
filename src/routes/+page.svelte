b09168cf67d04afbc018f1bb55ef331695ae68c8
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-12-30 12:37:10 +0900

    Refactor with `bind` and `style`.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 	import { Loader } from '@googlemaps/js-api-loader';
+
+	// Bindings
+	let mapElement: HTMLElement;
 
 	onMount(async function () {
 		const loader = new Loader({
@@ -10,22 +13,15 @@
 
 		const { Map } = await loader.importLibrary('maps');
 
-		let map = new Map(document.getElementById('map'), {
+		let map = new Map(mapElement, {
 			center: { lat: -34.397, lng: 150.644 },
 			zoom: 8
 		});
 	});
 </script>
 
-<div id="map"></div>
+<div bind:this={mapElement} style:height={'500px'} style:width={'800px'} />
 
 <svelte:head>
 	<script async src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 </svelte:head>
-
-<style>
-	#map {
-		height: 500px;
-		width: 800px;
-	}
-</style>
