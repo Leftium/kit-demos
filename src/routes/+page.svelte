0d0c75bf1cd8419c4671f506e7ca8b46b96d7e76
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-12-30 12:24:19 +0900

    Use @googlemaps/js-api-loader.
    - https://googlemaps.github.io/js-api-loader/index.html
    - https://github.com/googlemaps/js-api-loader/issues/482

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,37 @@
 <script>
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
+	import { Loader } from '@googlemaps/js-api-loader';
 
-    onMount(function() {
-        (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-            ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
+	onMount(async function () {
+		const loader = new Loader({
+			apiKey: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
+			version: 'weekly'
+		});
 
-        let map;
+		const { Map } = await loader.importLibrary('maps');
 
-        async function initMap() {
-            const { Map } = await google.maps.importLibrary("maps");
+		let map;
 
-            map = new Map(document.getElementById("map"), {
-                center: { lat: -34.397, lng: 150.644 },
-                zoom: 8,
-            });
-        }
+		async function initMap() {
+			map = new Map(document.getElementById('map'), {
+				center: { lat: -34.397, lng: 150.644 },
+				zoom: 8
+			});
+		}
 
-        initMap();
-    })
+		initMap();
+	});
 </script>
 
 <div id="map"></div>
 
 <svelte:head>
-    <script async src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+	<script async src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 </svelte:head>
 
 <style>
-    #map {
-        height: 500px;
-        width: 800px;
-    }
+	#map {
+		height: 500px;
+		width: 800px;
+	}
 </style>
