ddfd19ae1b5e8c2deee68e87fab45dccc762c6ee
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-02-17 14:32:04 +0900

    Add lightswitch component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-	import { Modal, getModalStore } from '@skeletonlabs/skeleton';
-	import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton';
-
-	import { initializeStores } from '@skeletonlabs/skeleton';
-
-	initializeStores();
-
-	const modalStore = getModalStore();
-
-	const modal: ModalSettings = {
-		type: 'alert',
-		// Data
-		title: 'Example Alert',
-		body: 'This is an example modal.',
-		image: 'https://i.imgur.com/WOgTG96.gif'
-	};
-	modalStore.trigger(modal);
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<Modal />
+<LightSwitch />
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
