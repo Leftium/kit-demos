85810c45fad7c5908958a78c5eee4fe885500073
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-01-12 21:32:40 +0900

    Add PicoCSS with dark theme.
    - In SPA mode (ssr = false)

---
+++ src/routes/+layout.ts
@@ -0,0 +1,9 @@
+// In conjunction with adapter-static, add prerender option to the root layout:
+// https://svelte.dev/docs/kit/adapter-static
+// https://svelte.dev/docs/kit/page-options#prerender
+export const prerender = true;
+
+// Turn any SvelteKit app into SPA by disabling SSR at the root layout:
+// https://svelte.dev/docs/kit/single-page-apps
+// https://svelte.dev/docs/kit/page-options#ssr
+export const ssr = false;
